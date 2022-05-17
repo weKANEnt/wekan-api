e29f7fba77619b261e9fb19c9ca9767e18fd7a5a
Your Naomi Benjamin <nobenjamin19@gmail.com>
2022-05-16 19:03:55 -0500

    Added enpoints for positions all voters vote for

--- src/app/ballotApp.js
+++ src/app/ballotApp.js
@@ -90,3 +90,262 @@ module.exports.getVPSSPCandidates = async function(req, res) {
         }
     }
 };
+
+/**
+ * Gets all candidates that are up for the VP SSP position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getVPPSICandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 3
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the Secretary position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getSecretaryCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 4
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the Secretary position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getTreasurerCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 5
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the GCC position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getGCCCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 6
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the PRO position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getPROCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 7
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the CEAC position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getCEACCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 8
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
+
+/**
+ * Gets all candidates that are up for the EAC position
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.getEACCandidates = async function(req, res) {
+    if (req.headers === null || req.headers === ""){
+        res.status(401).json(errorHandler.cannotAccess);
+    } else {
+        const token = getToken(req.headers);
+        const payload = await jwt.verify(token, config.jwt_key);
+
+        if (payload && payload.id) {
+            const voterr = await voter.isRegistered(payload.email);
+            if(!voterr){
+                    res.status(401).json(errorHandler.noVoter);
+                } else if (voterr) {
+                    const posNo = 9
+                    try {
+                        const candidates = await ballot.selectRequestedCandidates(posNo);
+                        if(candidates === 1){
+                            res.status(500).json(errorHandler.emptyParam);
+                        } else if (candidates){
+                            res.status(200).json(success(candidates, "VP SSP"));
+                        }
+                    }catch(err) {
+                        res.status(500).json(errorHandler.queryError);
+                    }
+                } else{
+                    res.status(500).json(errorHandler.serverError);
+                }
+        } else {
+            res.status(500).json(errorHandler.jwtError);
+        }
+    }
+};
--- src/routes/ballotRoute.js
+++ src/routes/ballotRoute.js
@@ -5,5 +5,12 @@ const router = express.Router();
 
 router.route("/presidents").get(bApp.getPresidentCandidates);
 router.route("/vpssp").get(bApp.getVPSSPCandidates);
+router.route("/vpssp").get(bApp.getVPPSICandidates);
+router.route("/secretary").get(bApp.getSecretaryCandidates);
+router.route("/treasurer").get(bApp.getTreasurerCandidates);
+router.route("/gcc").get(bApp.getGCCCandidates);
+router.route("/pro").get(bApp.getPROCandidates);
+router.route("/ceac").get(bApp.getCEACCandidates);
+router.route("/eac").get(bApp.getEACCandidates);
 
 module.exports = router;
