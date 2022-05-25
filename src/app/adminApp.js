e5f3ae682c05d55b3fd956a8f17080c778ed79a9
naomi-benjamin <104599886+naomi-benjamin@users.noreply.github.com>
2022-05-25 11:52:21 -0500

    jsDocs updates

--- src/app/adminApp.js
+++ src/app/adminApp.js
@@ -23,13 +23,13 @@ function success(token) {
  * @description Function to allow admin to login to admin portal to access admin functionality
  * @param {*} req
  * @param {*} res
- * @returns {Promise, 0, 1}
+ * @returns {Promise}
  */
 module.exports.loginAdmin = async function (req, res) {
   const { email, password } = req.body;
   if (!(email && password)) {
     res.status(503).json(errorHandler.emptyParam);
-    return 1;
+    return;
   }
   const vEmail = validate.valEmail(email);
   const vPassword = validate.valPassword(password);
@@ -54,21 +54,22 @@ module.exports.loginAdmin = async function (req, res) {
           );
           console.log(token)
           res.status(200).json(success(token));
+          return;
         }
       } else {
         res.status(500).json(errorHandler.serverError);
-        return 1;
+        return;
       }
     } catch (err) {
       res.status(500).json(errorHandler.jwtError);
-      return 1;
+      return;
     }
   } else if (vEmail !== true || vPassword !== true) {
     res.status(401).json(errorHandler.emailValidation);
-    return 1;
+    return;
   } else {
     res.status(500).json(errorHandler.serverError);
-    return 0;
+    return;
   }
 };
 
@@ -78,12 +79,13 @@ module.exports.loginAdmin = async function (req, res) {
  * @description Function to register a voter given that the given email is not currently registered
  * @param {*} req
  * @param {*} res
+ * @return {Promise}
  * Need to add that email should not be in admin table
  */
 module.exports.registerVoter = async function (req, res) {
   if (req.headers === null || req.headers === "") {
     res.status(401).json(errorHandler.cannotAccess);
-    return 1;
+    return;
   } else {
     const token = getToken(req.headers);
     const payload = await jwt.verify(token, config.jwt_key);
@@ -93,11 +95,11 @@ module.exports.registerVoter = async function (req, res) {
       const adminn = await admin.findAdminById(payload.id);
       if (!adminn) {
         res.status(401).json(errorHandler.noAdmins);
-        return 1;
+        return;
       } else if (adminn) {
         if ( !(email && hall && faculty  && doesCommute && isPostGrad)) {
           res.status(400).json(errorHandler.emptyParam);
-          return 1;
+          return;
         } else {
           const vEmail = validate.valEmail(email);
 
@@ -120,20 +122,20 @@ module.exports.registerVoter = async function (req, res) {
                 res
                   .status(200)
                   .json(successHandler(true, "Voter added successfully"));
-                return 0;
+                return;
               } else if (addVoter === 1) {
                 res.status(422).json(errorHandler.causingDuplicate);
-                return 1;
+                return;
               } else if (addVoter === 2) {
                 res.status(500).json(errorHandler.emptyParam);
-                return 1;
+                return;
               } else {
                 res.status(500).json(errorHandler.serverError);
-                return 1;
+                return;
               }
             } catch (err) {
               res.status(500).json(errorHandler.queryError);
-              return 1;
+              return;
             }
           } else if (
             vEmail != true ||
@@ -143,19 +145,19 @@ module.exports.registerVoter = async function (req, res) {
             !(isPostGrad === true || isPostGrad === false)
           ) {
             res.status(401).json(errorHandler.generalValidation);
-            return 1;
+            return;
           } else {
             res.status(500).json(errorHandler.serverError);
-            return 1;
+            return;
           }
         }
       } else {
         res.status(500).json(errorHandler.serverError);
-        return 1;
+        return;
       }
     } else {
       res.status(500).json(errorHandler.jwtError);
-      return 1;
+      return;
     }
   }
 };
@@ -166,11 +168,12 @@ module.exports.registerVoter = async function (req, res) {
  * @description Function to return all halls in the UWI
  * @param {*} req
  * @param {*} res
- * @return {Promise, 0, 1}
+ * @return {Promise}
  */
 module.exports.getAllHalls = async function (req, res) {
   if (req.headers === null || req.headers === "") {
     res.status(401).json(errorHandler.cannotAccess);
+    return;
   } else {
     const token = getToken(req.headers);
     const payload = await jwt.verify(token, config.jwt_key);
@@ -180,16 +183,20 @@ module.exports.getAllHalls = async function (req, res) {
         const adminn = await admin.findAdminById(payload.id);
         if (!adminn) {
           res.status(401).json(errorHandler.noAdmins);
+          return;
         } else if (adminn) {
           const allHalls = await admin.getAllHalls();
           if (allHalls) {
             res.status(200).json(successHandler(true, allHalls));
+            return;
           } else {
             res.status(500).json(errorHandler.serverError);
+            return;
           }
         }
       } catch (err) {
         res.status(500).json(errorHandler.serverError);
+        return;
       }
     }
   }
@@ -201,11 +208,12 @@ module.exports.getAllHalls = async function (req, res) {
  * @description Function to return all faculties in the UWI
  * @param {*} req
  * @param {*} res
- * @return {Promise, 0, 1}
+ * @return {Promise}
  */
 module.exports.getAllFaculties = async function (req, res) {
   if (req.headers === null || req.headers === "") {
     res.status(401).json(errorHandler.cannotAccess);
+    return;
   } else {
     const token = getToken(req.headers);
     const payload = await jwt.verify(token, config.jwt_key);
@@ -215,29 +223,37 @@ module.exports.getAllFaculties = async function (req, res) {
         const adminn = await admin.findAdminById(payload.id);
         if (!adminn) {
           res.status(401).json(errorHandler.noAdmins);
+          return;
         } else if (adminn) {
           const allFaculties = await admin.getAllFaculties();
           if (allFaculties) {
             res.status(200).json(successHandler(true, allFaculties));
+            return;
           } else {
             res.status(500).json(errorHandler.serverError);
+            return;
           }
         }
       } catch (err) {
         res.status(500).json(errorHandler.serverError);
+        return;
       }
     }
   }
 };
 
 /**
- *
+ * @function
+ * @name getAllPositions
+ * @description gets all the positions for the uwi guild election
  * @param {*} req
  * @param {*} res
+ * @return {Promise}
  */
 module.exports.getAllPositions = async function (req, res) {
   if (req.headers === null || req.headers === "") {
     res.status(401).json(errorHandler.cannotAccess);
+    return;
   } else {
     const token = getToken(req.headers);
     const payload = await jwt.verify(token, config.jwt_key);
@@ -247,16 +263,20 @@ module.exports.getAllPositions = async function (req, res) {
         const adminn = await admin.findAdminById(payload.id);
         if (!adminn) {
           res.status(401).json(errorHandler.noAdmins);
+          return;
         } else if (adminn) {
           const allPositions = await admin.getAllPositions();
           if (allPositions) {
             res.status(200).json(successHandler(true, allPositions));
+            return;
           } else {
             res.status(500).json(errorHandler.serverError);
+            return;
           }
         }
       } catch (err) {
         res.status(500).json(errorHandler.serverError);
+        return;
       }
     }
   }
@@ -268,11 +288,12 @@ module.exports.getAllPositions = async function (req, res) {
  * @description Function to add a people who are up for a position in the election - aka a candidates
  * @param {*} req
  * @param {*} res
- * @return {Promise, 0, 1}
+ * @return {Promise}
  */
 module.exports.addCandidate = async function (req, res) {
   if (req.headers === null || req.headers === "") {
     res.status(401).json(errorHandler.cannotAccess);
+    return;
   } else {
     const token = getToken(req.headers);
     const payload = await jwt.verify(token, config.jwt_key);
@@ -282,6 +303,7 @@ module.exports.addCandidate = async function (req, res) {
       const adminn = await admin.findAdminById(payload.id);
       if (!adminn) {
         res.status(401).json(errorHandler.noAdmins);
+        return;
       } else if (adminn) {
         for (let c = 0; c < candidates.length; c++) {
           if (
@@ -296,6 +318,7 @@ module.exports.addCandidate = async function (req, res) {
             )
           ) {
             res.status(400).json(errorHandler.emptyFields);
+            return;
           } else {
             const vEmail = validate.valEmail(candidates[c].email);
             const vFname = validate.valName(candidates[c].firstName);
@@ -315,14 +338,16 @@ module.exports.addCandidate = async function (req, res) {
                   candidates[c].about
                 );
                 addSum = addSum + addCandidate;
-                console.log(addCandidate);
               } catch (err) {
                 res.status(500).json(errorHandler.queryError);
+                return;
               }
             } else if (!vEmail || !vFname || !vLname) {
               res.status(401).json(errorHandler.genValidation);
+              return;
             } else {
               res.status(500).json(errorHandler.serverError);
+              return;
             }
           }
         }
@@ -330,10 +355,13 @@ module.exports.addCandidate = async function (req, res) {
           res
             .status(200)
             .json(successHandler(true, "All candidates added successfully"));
+          return;
         } else if (addSum < 0) {
           res.status(400).json(errorHandler.serverError);
+          return;
         } else {
           res.status(400).json(errorHandler.cannotAddCandidate);
+          return;
         }
       }
     }
